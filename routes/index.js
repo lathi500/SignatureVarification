634aeec918467233b1a47ec8d868af6dacdf16d4
bhavin lathiya <bhavin.lathiya@codezeros.com>
2022-07-10 18:17:28 +0530

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,108 +1,108 @@
 var express = require('express');
 var router = express.Router();
 const crypto = require('crypto');
-const { type } = require('express/lib/response');
-const solc = require('solc');
-Web3  = require('web3');
+Web3 = require('web3');
 fs = require('fs');
+const Provider = require('@truffle/hdwallet-provider');
 
-web3 = new Web3( new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
-
+web3 = new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
+var SmartContractAddress = "0xfb444d53612a7d1ae4b1ed66aea5ea818003bac2";
+var contract_abi = require('./abi.json');
+var SmartContractABI = contract_abi;
+var address = "0x33c1d0fe689589cddbafc68a53afdec0cfa52f9a"
+var privatekey = "fc1a82dd3fcbe4d94d1da4145f47896e756abbe004dd40ee72caf8c9ee29b78d";
+var rpcurl = "https://ropsten.infura.io/v3/061c56b4611d495186f1b65c7ef5e37c";
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/generate-key', (req, res)  => {
+router.get('/generate-key', (req, res) => {
 
-  const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa',{
-  modulusLength: 2048,
-  publicKeyEncoding: {
-    type: 'spki',
-    format: 'der',
-  },
-  privateKeyEncoding: {
+  const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+    modulusLength: 2048,
+    publicKeyEncoding: {
+      type: 'spki',
+      format: 'der',
+    },
+    privateKeyEncoding: {
+      type: 'pkcs8',
+      format: 'der',
+    },
+
+  })
+
+  res.send({ publicKey: publicKey.toString('base64'), privateKey: privateKey.toString('base64') });
+
+});
+
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/sign', (req, res) => {
+  let privateKey = req.body.privateKey;
+  let data = req.body.data;
+
+
+  privateKey = crypto.createPrivateKey({
+    key: Buffer.from(privateKey, 'base64'),
     type: 'pkcs8',
     format: 'der',
-  },
+  })
 
- })
- 
- res.send({ publicKey: publicKey.toString('base64'), privateKey: privateKey.toString('base64') }); 
-
-});
-
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.post('/sign', (req, res)  => {
-  let privateKey = req.body.privateKey;
-    let data = req.body.data;
-  
-
-    privateKey = crypto.createPrivateKey({
-      key: Buffer.from(privateKey, 'base64'),
-      type: 'pkcs8',
-      format: 'der',
-    })
-
-    const sign =  crypto.createSign('SHA256')
-    sign.update(data),
+  const sign = crypto.createSign('SHA256')
+  sign.update(data),
     sign.end()
 
-    const signature = sign.sign(privateKey).toString('base64')
+  const signature = sign.sign(privateKey).toString('base64')
 
-    res.send({  signature });
+  res.send({ signature });
 
- })
-
- router.post('/verify', (req, res)  => {
-    let{ data, publicKey, signature } = req.body;
-
-
-    publicKey = crypto.createPublicKey({
-      key: Buffer.from(publicKey, 'base64'),
-      type: 'pkcs8',
-      format: 'der',
-    })
-
-    const verify = crypto.createVerify("SHA256")
-    verify.update(data),
-    verify.end()
-
-    let result = verify.verify(publicKey, Buffer.from(signature, 'base64'));
-
-    res.send( { ans: result });
-
- })
-
-
+})
 
 module.exports = router;
-var input = {
-  language: "Solidity",
-  sources: {
-    "contract/Signature_varification.sol": {
-      content: file,
-    },
-  },
+
+router.post('/verify', (req, res) => {
+  let { data, publicKey, signature } = req.body;
+  var provider = new Provider(privatekey, rpcurl);
+  var web3 = new Web3(provider);
+  var myContract = new web3.eth.Contract(SmartContractABI, SmartContractAddress);
+
+  publicKey = crypto.createPublicKey({
+    key: Buffer.from(publicKey, 'base64'),
+    type: 'pkcs8',
+    format: 'der',
+  })
+
+  let result = myContract.methods.verify(publicKey, data, signature).send({ from: address });
   
-  settings: {
-    outputSelection: {
-      "*": {
-        "*": ["*"],
-      },
-    },
-  },
-};
-  
-var output = JSON.parse(solc.compile(JSON.stringify(input)));
-console.log("Result : ", output);
-  
-ABI = output.contracts["contract/Signature_varification.sol"]["VarifySign"].abi;
-bytecode = output.contracts["contract/Signature_varification.sol"]["VarifySign"].evm.bytecode.object;
-console.log("Bytecode: ", bytecode);
-console.log("ABI: ", ABI);
+  res.send({ ans: result });
+})
+
+console.log('Hello')
+
+// const sendData = async () => {
+
+//   var provider = new Provider(privatekey, rpcurl);
+//   var web3 = new Web3(provider);
+//   var myContract = new web3.eth.Contract(SmartContractABI, SmartContractAddress);
+//   var oldvalue = await myContract.methods.retrieve().call();
+//   console.log("oldvalue", oldvalue);
+
+
+//   var receipt = await myContract.methods.store(5782).send({ from: address });
+//   console.log(receipt);
+
+//   var newvalue = await myContract.methods.retrieve().call();
+//   console.log("newvalue", newvalue);
+
+//   console.log("done with all things");
+
+// }
+
+// sendData();
+
+
